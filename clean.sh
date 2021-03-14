5a65c52f8d45e2d66550356b8d1d885d24679c4e
uty <whensungoes@gmail.com>
2021-03-14 16:24:34 -0400

    change to IEEE.cls 1.8b

---
+++ clean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm *.aux *.bbl *.fdb_latexmk *.fls *.log *.pdf *.synctex* *.blg *.out
+rm sources/*.aux sources/*.bbl sources/*.fdb_latexmk sources/*.fls sources/*.log sources/*.pdf sources/*.synctex* sources/*.blg sources/*.out
+rm common/*.aux common/*.bbl common/*.fdb_latexmk common/*.fls common/*.log common/*.pdf common/*.synctex* common/*.blg common/*.out
+
